bd2dae4d4adc3e41c5c7ce6e160cda0e05f8c433
Sam Santomartino <sms063@bucknell.edu>
2016-11-02 16:42:27 -0400

    Committing initial Frogger Main

---
+++ csci205FinalProject/src/FroggerMain.java
@@ -0,0 +1,59 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Fall 2016
+*
+* Name: Sam Santomartino, Grace Cook, and John O'Brien
+* Date: Nov 2, 2016
+* Time: 4:41:44 PM
+*
+* Project: csci205FinalProject
+* Package:
+* File: FroggerMain
+* Description:
+*
+* ****************************************
+ */
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sms063
+ */
+public class FroggerMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
